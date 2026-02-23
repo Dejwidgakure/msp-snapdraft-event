88069def0f16ceb9b471a24561c861ae7525545a
Dejwidgakure <ksiazedejwid@gmail.com>
2026-02-23 17:36:39 +0100

    Update draft18-data.js

--- draft18-data.js
+++ draft18-data.js
@@ -9,10 +9,11 @@ const players = [
   "Matixmgg",
   "Fizjoradek",
   "Kmythic",
-  "Maniek"
+  "Maniek",
 ];
 
 const matches = [
   { p1: "Dejwid", p2: "Maniek", pts1: 22, pts2: 3 },
-  { p1: "Dejwid", p2: "Dawid ak. Void", pts1: 6, pts2: 19 }
+  { p1: "Dejwid", p2: "Dawid ak. Void", pts1: 6, pts2: 19 },
+  { p1: "Dawid ak. Void", p2: "Budiso", pts1: 24, pts2: 1 },
 ];
