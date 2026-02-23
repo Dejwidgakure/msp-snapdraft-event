4f4204f5137e09e280bc3da6cfb4f1266f5be03c
Dejwidgakure <ksiazedejwid@gmail.com>
2026-02-23 17:43:29 +0100

    Update draft18-data.js

--- draft18-data.js
+++ draft18-data.js
@@ -16,4 +16,8 @@ const matches = [
   { p1: "Dejwid", p2: "Maniek", pts1: 22, pts2: 3 },
   { p1: "Dejwid", p2: "Dawid ak. Void", pts1: 6, pts2: 19 },
   { p1: "Dawid ak. Void", p2: "Budiso", pts1: 24, pts2: 1 },
+  { p1: "Raciatek", p2: "Budiso", pts1: 18, pts2: 7 },
+  { p1: "Dawid ak. Void", p2: "Maniek", pts1: 21, pts2: 4 },
+  { p1: "Raciatek", p2: "Fizjoradek", pts1: 21, pts2: 4 },
+  { p1: "Kmythic", p2: "Weregesu", pts1: 25, pts2: 0 },
 ];
