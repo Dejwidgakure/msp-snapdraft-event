73322803573cc5c1db78ee0daf801c40f634f5fc
Dejwidgakure <ksiazedejwid@gmail.com>
2026-02-23 17:52:13 +0100

    Update draft18-data.js

--- draft18-data.js
+++ draft18-data.js
@@ -20,4 +20,12 @@ const matches = [
   { p1: "Dawid ak. Void", p2: "Maniek", pts1: 21, pts2: 4 },
   { p1: "Raciatek", p2: "Fizjoradek", pts1: 21, pts2: 4 },
   { p1: "Kmythic", p2: "Weregesu", pts1: 25, pts2: 0 },
+  { p1: "Fizjoradek", p2: "Maniek", pts1: 17, pts2: 8 },
+  { p1: "Weregesu", p2: "Maniek", pts1: 24, pts2: 1 },
+  { p1: "Fizjoradek", p2: "Dawid ak. Void", pts1: 21, pts2: 4 },
+  { p1: "Kmythic", p2: "Maniek", pts1: 23, pts2: 2 },
+  { p1: "Weregesu", p2: "Raciatek", pts1: 21, pts2: 4 },
+  { p1: "Kmythic", p2: "Dawid ak. Void", pts1: 21, pts2: 4 },
+  { p1: "Fizjoradek", p2: "Kmythic", pts1: 21, pts2: 4 },
+  { p1: "Fizjoradek", p2: "Weregesu", pts1: 22, pts2: 3 },
 ];
