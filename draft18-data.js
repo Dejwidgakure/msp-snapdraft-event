8cafa5f541bdfd013aa90fe081ad3fb5f124d9a4
Dejwidgakure <ksiazedejwid@gmail.com>
2026-02-23 16:46:23 +0100

    Create draft18-data.js

---
+++ draft18-data.js
@@ -0,0 +1,18 @@
+const players = [
+  "Dejwid",
+  "Void",
+  "Budiso",
+  "Pluton",
+  "Raciatek",
+  "Supcio",
+  "Weregesu",
+  "Matix",
+  "Fizjoradek",
+  "Kmythic",
+  "Maniek"
+];
+
+const matches = [
+  // PRZYKŁAD:
+  // { p1: "Dejwid", p2: "Maniek", pts1: 21, pts2: 4 },
+];
