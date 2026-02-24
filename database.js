59ba660839c9d8cc9e0223f476537e20f1dba90a
Dejwidgakure <ksiazedejwid@gmail.com>
2026-02-24 12:41:40 +0100

    Update database.js

--- database.js
+++ database.js
@@ -2,28 +2,52 @@ const database = {
   drafts: [
     {
       id: 18,
-      startDate: "2026-02-20",
-      endDate: "2026-02-20",
-      mode: "Poker",
+      startDate: "2026-02-21",
+      endDate: "2026-03-06",
+      mode: "Classic + Bany + Jokery",
       presetCard: null,
       playersCount: 11,
       winner: null,
+       bans: [
+    "Alioth",
+    "Shang-Chi"
+  ],
 
       players: [
-        { name: "Dejwid", deck: [] },
-        { name: "Dawid ak. Void", deck: [] },
-        { name: "Budiso", deck: [] },
-        { name: "Pluton", deck: [] },
-        { name: "Raciatek", deck: [] },
-        { name: "Supcio", deck: [] },
-        { name: "Weregesu", deck: [] },
-        { name: "Matixmgg", deck: [] },
-        { name: "Fizjoradek", deck: [] },
-        { name: "Kmythic", deck: [] },
-        { name: "Maniek", deck: [] }
+        { name: "Dejwidgakure", deck: ["Black Knight","Uatu the Watcher","Nova Frankie Raye","Nocturne","Prodigy","Omega Sentinel","Miles Morales Spider-Man","Doctor Octopus","Crossbones","osmic Ghost Rider","Mockingbird","Thanos"] },
+        { name: "Dawid ak. Void", deck: ["Nova","X-23","Carnage","Weapon X Wolverine","Ironheart","Venom","Adamantium Infiusion","Danger","Nimrod","Blob","Destroyer","Death"] },
+        { name: "Budiso", deck: ["Clea","Bruce Bunner","Merlin","Master Mold","Doctor Strange","Bishop","Silver Surfer First Steps","Gwenpool","MoonGirl","Silver Samurai","Profesor X","Doctor Doom"] },
+        { name: "Pluton32", deck: ["Sauron","Ghost-Spider","Vulture","Merlin","U.S. Agent","Brood","Nocturne","Prodigy","Dracula","Misery","Galactus","The Infinaut"] },
+        { name: "Raciatek", deck: ["Agony","Fastball Special","Morbius","Lasher","The First Ghost Rider","Human Torch","Gambit","Silver Surfer First Steps","Cull Obsidian","Legion","Blob","Hulk"] },
+        { name: "Supcio", deck: ["Martyr","Domino","Toxin","Air-Walker","Debrii","Phastos","Sword Master","Anti-Venom","Crossbones","Black Bolt","Hobgoblin","Thanos"] },
+        { name: "Weregesu", deck: ["Arana","Nico Minoru","Squirrel Girl","Hydra Bob","Mister Sinister","Scarlet Witch","Colossus","Marvel Boy","Copycat","Redwing","Sword Master","Giganto"] },
+        { name: "Matixmgg", deck: ["Vulture","Forge","Lockjaw","Domino","Brood","Human Torch First Steps","Black Widow","Gladiator","Jessica Jones","Laufey","Stardust","Hellion"] },
+        { name: "Fizjoradek", deck: ["Elektra","Misty Knight","Victoria Hand","Jennifer Kale","Crystal","Kang The Conqueror","Zombie Scarlet Witch","Fan Fei","Negasonic Teenage Warhead","Anti-Venom","Cosmic Ghost Rider","Ajax"] },
+        { name: "Kmythic", deck: ["Forge","Jennifer Kale","Red Shift","Toxie Doxie","Kraglin","Mister Negative","Ghost Rider","Darkhawk","M.O.D.O.K.","Apocalypse","Agatha Harkness","Zombie Galacti"] },
+        { name: "Maniek", deck: ["Ghost-Spider","Grand Master","Sentinel","Cable","Captain America","Rogue","Mole Man","Nakia","Gladiator","Namor","Mockingbird","Odin"] }
       ],
 
-      matches: []
-    }
-  ]
+      matches: [
+  { p1: "Dejwid", p2: "Maniek", pts1: 22, pts2: 3 },
+  { p1: "Dejwid", p2: "Dawid ak. Void", pts1: 6, pts2: 19 },
+  { p1: "Dawid ak. Void", p2: "Budiso", pts1: 24, pts2: 1 },
+  { p1: "Raciatek", p2: "Budiso", pts1: 18, pts2: 7 },
+  { p1: "Dawid ak. Void", p2: "Maniek", pts1: 21, pts2: 4 },
+  { p1: "Raciatek", p2: "Fizjoradek", pts1: 21, pts2: 4 },
+  { p1: "Kmythic", p2: "Weregesu", pts1: 25, pts2: 0 },
+  { p1: "Fizjoradek", p2: "Maniek", pts1: 17, pts2: 8 },
+  { p1: "Weregesu", p2: "Maniek", pts1: 24, pts2: 1 },
+  { p1: "Fizjoradek", p2: "Dawid ak. Void", pts1: 21, pts2: 4 },
+  { p1: "Kmythic", p2: "Maniek", pts1: 23, pts2: 2 },
+  { p1: "Weregesu", p2: "Raciatek", pts1: 21, pts2: 4 },
+  { p1: "Kmythic", p2: "Dawid ak. Void", pts1: 21, pts2: 4 },
+  { p1: "Fizjoradek", p2: "Kmythic", pts1: 21, pts2: 4 },
+  { p1: "Fizjoradek", p2: "Weregesu", pts1: 22, pts2: 3 },
+  { p1: "Kmythic", p2: "Raciatek", pts1: 18, pts2: 7 },
+  { p1: "Dejwid", p2: "Kmythic", pts1: 22, pts2: 3 },
+  { p1: "Weregesu", p2: "Budiso", pts1: 23, pts2: 2 },
+  { p1: "Kmythic", p2: "Budiso", pts1: 25, pts2: 0 }
+        ]
+      }
+    ]
 };
