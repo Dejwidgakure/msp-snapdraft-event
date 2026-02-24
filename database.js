07876eade6f8ad2e82a9dca7e6b3635cf63bc91d
Dejwidgakure <ksiazedejwid@gmail.com>
2026-02-24 10:39:08 +0100

    Create database.js

---
+++ database.js
@@ -0,0 +1,31 @@
+const database = {
+  drafts: [
+    {
+      id: 18,
+
+      startDate: "2026-02-20",
+      endDate: "2026-02-20",
+
+      mode: "Poker",
+      presetCard: null,
+      playersCount: 11,
+      winner: null,
+
+      players: [
+        { name: "Dejwid", deck: [] },
+        { name: "Dawid ak. Void", deck: [] },
+        { name: "Budiso", deck: [] },
+        { name: "Pluton", deck: [] },
+        { name: "Raciatek", deck: [] },
+        { name: "Supcio", deck: [] },
+        { name: "Weregesu", deck: [] },
+        { name: "Matixmgg", deck: [] },
+        { name: "Fizjoradek", deck: [] },
+        { name: "Kmythic", deck: [] },
+        { name: "Maniek", deck: [] }
+      ],
+
+      matches: []
+    }
+  ]
+};
