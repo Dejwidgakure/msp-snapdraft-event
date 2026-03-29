adbe3c3bded3f7c37741c58c42210e8b3ba05d9d
Dejwidgakure <ksiazedejwid@gmail.com>
2026-03-29 21:03:15 +0200

    Update database.js

--- database.js
+++ database.js
@@ -1117,7 +1117,7 @@ matches:[
                 {"p1":"John Skrull","p2":"Budiso","pts1":25,"pts2":0},{"p1":"Kmythic","p2":"Dejwidgakure","pts1":23,"pts2":2},{"p1":"Raciatek","p2":"Budiso","pts1":0,"pts2":25},
                 {"p1":"John Skrull","p2":"Raciatek","pts1":23,"pts2":2},{"p1":"Supcio","p2":"Raciatek","pts1":24,"pts2":1},{"p1":"Raciatek","p2":"Kmythic","pts1":0,"pts2":25},
                 {"p1":"Dejwidgakure","p2":"Weregesu","pts1":20,"pts2":5},{"p1":"Kmythic","p2":"Skaje","pts1":23,"pts2":2},{"p1":"John Skrull","p2":"Skaje","pts1":25,"pts2":0},
-                {"p1":"Supcio","p2":"Dawid ak. Void","pts1":22,"pts2":3},{"p1":"Hardkor","p2":"Raciatek","pts1":20,"pts2":5}]
+                {"p1":"Supcio","p2":"Dawid ak. Void","pts1":22,"pts2":3},{"p1":"Hardkor","p2":"Raciatek","pts1":20,"pts2":5},{"p1":"Weregesu","p2":"Raciatek","pts1":19,"pts2":6}]
 
 }
     
