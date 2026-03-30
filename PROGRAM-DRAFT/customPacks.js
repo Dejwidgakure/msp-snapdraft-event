aaebfa4127496a5b01a9d143c24cf6ca7c80cc64
Dejwidgakure <ksiazedejwid@gmail.com>
2026-03-30 14:11:34 +0200

    Add files via upload

---
+++ PROGRAM-DRAFT/customPacks.js
@@ -0,0 +1,98 @@
+// ===============================
+// 🎴 CUSTOM PACK DEFINITIONS
+// ===============================
+
+const customPacks = [
+
+    {
+        id: "jubilee_1_2",
+        name: "JUBILEUSZ DRAFT 1 & 2 - Hahke & Dejwidgakure",
+        tags: ["wind1", "wind2"]
+    },
+
+    {
+        id: "jubilee_3_4",
+        name: "JUBILEUSZ DRAFT 3 & 4 - kb & Dejwidgakure",
+        tags: ["wind3", "wind4"]
+    },
+
+    {
+        id: "jubilee_5_6",
+        name: "JUBILEUSZ DRAFT 5 & 6 - Xevo & kb",
+        tags: ["wind5", "wind6"]
+    },
+
+    {
+        id: "jubilee_7_8",
+        name: "JUBILEUSZ DRAFT 7 & 8 - Marek & Jacusinski",
+        tags: ["wind7", "wind8"]
+    },
+
+    {
+        id: "jubilee_9_10",
+        name: "JUBILEUSZ DRAFT 9 & 10 - Jacusinski & Hardkor",
+        tags: ["wind9", "wind10"]
+    },
+
+    {
+        id: "jubilee_11_12",
+        name: "JUBILEUSZ DRAFT 11 & 12 - Raciatek & Polop582",
+        tags: ["wind11", "wind12"]
+    },
+
+    {
+        id: "jubilee_13_14",
+        name: "JUBILEUSZ DRAFT 13 & 14 - Supcio & Dejwidgakure",
+        tags: ["wind13", "wind14"]
+    },
+
+    {
+        id: "jubilee_15_16",
+        name: "JUBILEUSZ DRAFT 15 & 16 - Supcio & Weregesu",
+        tags: ["wind15", "wind16"]
+    },
+
+    {
+        id: "jubilee_17_18",
+        name: "JUBILEUSZ DRAFT 17 & 18 - Supcio & Kmythic",
+        tags: ["wind17", "wind18"]
+    },
+ {
+        id: "jubilee_19",
+        name: "JUBILEUSZ DRAFT 19 - Dejwidgakure",
+        tags: ["wind19"]
+    },
+
+{
+    id: "animal_pack",
+    name: "ANIMAL PACK 🐾",
+    tags: ["animal"]
+},
+
+{
+    id: "avengers_pack",
+    name: "PACZKA AVENGERS",
+    tags: ["avengers"]
+},
+{
+    id: "low_cost",
+    name: "LOW COST",
+    minCost: 0,
+    maxCost: 2
+},
+{
+    id: "hybrid_pack",
+    name: "MOCNE & TANIE",
+    tags: [],
+    minCost: 2,
+    maxCost: 3,
+    minPower: 5
+},
+{
+    id: "red_pack",
+    name: "RED PACK 🔴",
+    nameIncludes: "red"
+}
+
+
+];
