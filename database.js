2ea99beb911c926c6e5b2bc12b64d69476b4bb6d
Dejwidgakure <ksiazedejwid@gmail.com>
2026-04-24 23:12:47 +0200

    Update database.js

--- database.js
+++ database.js
@@ -1355,7 +1355,7 @@ matches:[
 			   {"p1":"BSTHPRO","p2":"Supcio","pts1":0,"pts2":20},{"p1":"FizjoRadek","p2":"BSTHPRO","pts1":20,"pts2":0},{"p1":"BSTHPRO","p2":"Maniek","pts1":0,"pts2":20},
 			   {"p1":"BSTHPRO","p2":"Raciatek","pts1":0,"pts2":20},{"p1":"Supcio","p2":"Raciatek","pts1":19,"pts2":6},{"p1":"Dejwidgakure","p2":"Supcio","pts1":21,"pts2":4},
 			   {"p1":"Matixmgg","p2":"Raciatek","pts1":2,"pts2":23},{"p1":"Matixmgg","p2":"Budiso","pts1":19,"pts2":6},{"p1":"Matixmgg","p2":"Supcio","pts1":1,"pts2":24},
-			   {"p1":"MatimyHair","p2":"Dejwidgakure","pts1":25,"pts2":0},{"p1":"Dejwidgakure","p2":"Raciatek","pts1":22,"pts2":3},{"p1":"Matixmgg","p2":"FizjoRadek","pts1":0,"pts2":20},
+			   {"p1":"MatiMyHair","p2":"Dejwidgakure","pts1":25,"pts2":0},{"p1":"Dejwidgakure","p2":"Raciatek","pts1":22,"pts2":3},{"p1":"Matixmgg","p2":"FizjoRadek","pts1":0,"pts2":20},
 			   {"p1":"Matixmgg","p2":"Kmythic","pts1":0,"pts2":20}
 				
               ]
