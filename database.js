1f79d04c5d6eb085272981daa6188f51c9dd40b0
Dejwidgakure <ksiazedejwid@gmail.com>
2026-05-07 11:17:07 +0200

    Update database.js

--- database.js
+++ database.js
@@ -1586,7 +1586,7 @@ matches:[
 			   {"p1":"Kmythic","p2":"Maniek","pts1":24,"pts2":1},{"p1":"FizjoRadek","p2":"Kmythic","pts1":22,"pts2":3},{"p1":"Kmythic","p2":"Weregesu","pts1":20,"pts2":5},
 			   {"p1":"Weregesu","p2":"Budiso","pts1":25,"pts2":0},{"p1":"FizjoRadek","p2":"Budiso","pts1":25,"pts2":0},{"p1":"Budiso","p2":"Maniek","pts1":21,"pts2":4},
 			   {"p1":"MatiMyHair","p2":"Kmythic","pts1":22,"pts2":3},{"p1":"MatiMyHair","p2":"Budiso","pts1":25,"pts2":0},{"p1":"FizjoRadek","p2":"kamajot","pts1":19,"pts2":6},
-			   {"p1":"kamajotr","p2":"Kmythic","pts1":19,"pts2":6},{"p1":"FizjoRadek","p2":"Weregesu","pts1":22,"pts2":3},
+			   {"p1":"kamajot","p2":"Kmythic","pts1":19,"pts2":6},{"p1":"FizjoRadek","p2":"Weregesu","pts1":22,"pts2":3},
 			   {"p1":"MatiMyHair","p2":"Dejwidgakure","pts1":20,"pts2":0},{"p1":"Maniek","p2":"Dejwidgakure","pts1":20,"pts2":0},{"p1":"FizjoRadek","p2":"Dejwidgakure","pts1":20,"pts2":0},
 			   {"p1":"Supcio","p2":"Dejwidgakure","pts1":20,"pts2":0},{"p1":"kamajot","p2":"Dejwidgakure","pts1":20,"pts2":0},{"p1":"Kmythic","p2":"Dejwidgakure","pts1":20,"pts2":0},
 			   {"p1":"Weregesu","p2":"Dejwidgakure","pts1":20,"pts2":0},{"p1":"Budiso","p2":"Dejwidgakure","pts1":20,"pts2":0},{"p1":"SalsaTequilaJr","p2":"Dejwidgakure","pts1":20,"pts2":0}
