15ad9abde72fa9cbf6887fe29245325172eeed5c
Dejwidgakure <ksiazedejwid@gmail.com>
2026-05-17 01:33:32 +0200

    Update jokerEngine.js

--- PROGRAM-DRAFT/jokerEngine.js
+++ PROGRAM-DRAFT/jokerEngine.js
@@ -1,1 +1,19 @@
-witam
+function getRandomJoker(){
+
+    return jokers[
+        Math.floor(Math.random() * jokers.length)
+    ];
+
+}
+
+function isChoiceJoker(joker){
+
+    return joker.type === "choice";
+
+}
+
+function isSurpriseJoker(joker){
+
+    return joker.type === "surprise";
+
+}
