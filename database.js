0cc3ec80474597f8c518bc987514ba3d2f98a7d8
Dejwidgakure <ksiazedejwid@gmail.com>
2026-07-02 22:26:26 +0200

    Update database.js

--- database.js
+++ database.js
@@ -2020,6 +2020,8 @@ matches:[
 			   {"p1":"Raciatek","p2":"Weregesu","pts1":2,"pts2":23},{"p1":"Supcio","p2":"Maniek","pts1":0,"pts2":25},{"p1":"MatiMyHair","p2":"Budiso","pts1":20,"pts2":0},
 			   {"p1":"Budiso","p2":"Weregesu","pts1":0,"pts2":20},{"p1":"Supcio","p2":"Budiso","pts1":20,"pts2":0},{"p1":"Raciatek","p2":"Budiso","pts1":20,"pts2":0},
 			   {"p1":"Budiso","p2":"kamajot","pts1":0,"pts2":20},{"p1":"Dejwidgakure","p2":"Budiso","pts1":20,"pts2":0},{"p1":"Raciatek","p2":"Budiso","pts1":20,"pts2":0},
+			   {"p1":"Supcio","p2":"kamajot","pts1":25,"pts2":0},{"p1":"Supcio","p2":"Raciatek","pts1":21,"pts2":4},{"p1":"Dejwidgakure","p2":"Supcio","pts1":20,"pts2":5},
+			   {"p1":"Dejwidgakure","p2":"MatiMyHair","pts1":21,"pts2":4},{"p1":"MatiMyHair","p2":"Weregesu","pts1":25,"pts2":0},{"p1":"Dejwidgakure","p2":"kamajot","pts1":20,"pts2":0}
 			]
 
 }
