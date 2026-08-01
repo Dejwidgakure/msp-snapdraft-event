06af41737b019d7f2656f0f6136278dd24070245
Dejwidgakure <ksiazedejwid@gmail.com>
2026-08-01 20:07:03 +0200

    Update database.js

--- database.js
+++ database.js
@@ -2531,7 +2531,7 @@ matches:[
 			  {"p1":"Matixmgg","p2":"MatiMyHair","pts1":0,"pts2":25}, {"p1":"Budiso","p2":"Weregesu","pts1":21,"pts2":4},
 			  {"p1":"Dejwidgakure","p2":"MatiMyHair","pts1":23,"pts2":2}, {"p1":"Budiso","p2":"Kmythic","pts1":8,"pts2":17},
 			  {"p1":"Dejwidgakure","p2":"Kmythic","pts1":5,"pts2":20}, {"p1":"Budiso","p2":"Dejwidgakure","pts1":1,"pts2":24},
-			  {"p1":"Budiso","p2":"MatiMyHair","pts1":25,"pts2":0}, {"p1":"MatiMyHair","p2":"Kmythic","pts1":20,"pts2":5},
+			  {"p1":"Budiso","p2":"MatiMyHair","pts1":0,"pts2":25}, {"p1":"MatiMyHair","p2":"Kmythic","pts1":20,"pts2":5},
 			  {"p1":"Matixmgg","p2":"Kmythic","pts1":24,"pts2":1}, {"p1":"Matixmgg","p2":"Supcio","pts1":0,"pts2":25}
 			  
 			  
