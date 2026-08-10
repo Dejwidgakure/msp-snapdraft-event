68d0adbeb768ab061b82ad54866465429181b035
Dejwidgakure <ksiazedejwid@gmail.com>
2026-08-10 22:42:39 +0200

    Update database.js

--- database.js
+++ database.js
@@ -2805,8 +2805,8 @@ matches:[
     {"p1":"Dejwidgakure","p2":"FizjoRadek","pts1":22,"pts2":3},{"p1":"Qrls","p2":"MatiMyHair","pts1":16,"pts2":9},
     {"p1":"Budiso","p2":"Supcio","pts1":21,"pts2":4},{"p1":"Maniek","p2":"MatiMyHair","pts1":0,"pts2":25},
 	{"p1":"Weregesu","p2":"Supcio","pts1":25,"pts2":0},{"p1":"FizjoRadek","p2":"Matixmgg","pts1":25,"pts2":0},
-	{"p1":"Matixmgg","p2":"Supcio","pts1":4,"pts2":21},{"p1":"Maniek","p2":"Matixmgg","pts1":0,"pts2":25}
-	
+	{"p1":"Matixmgg","p2":"Supcio","pts1":4,"pts2":21},{"p1":"Maniek","p2":"Matixmgg","pts1":0,"pts2":25},
+	{"p1":"Dejwidgakure","p2":"Qrls","pts1":16,"pts2":9}
 
 
 	
